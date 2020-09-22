498df79e3d6fbe38bec01a5ff48a36545151d768
DanielMayo2458 <daniel.mayo@alumnos.udg.mx>
2020-09-21 22:34:16 -0500

    archivos R

---
+++ Tarea1.R
@@ -0,0 +1,7 @@
+test=read.csv("C:/Users/danie/OneDrive/Documentos/Materias del MNEE/2do Semestre/Metodos Cuantitativos de Analisis II/Github/Tarea1/Tarea1/test.csv")
+test
+
+x = test$Precio
+y= test$Superficie
+
+plot(x,y)
