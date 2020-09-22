7f42a29705322e156119583e9ae84fccafca3101
yuri8207 <yuriany.salgado@gmail.com>
2020-09-21 23:35:27 -0500

    Cambios en código

--- Tarea1.R
+++ Tarea1.R
@@ -1,7 +1,26 @@
-test=read.csv("C:/Users/danie/OneDrive/Documentos/Materias del MNEE/2do Semestre/Metodos Cuantitativos de Analisis II/Github/Tarea1/Tarea1/test.csv")
+test=read.csv("D:/yuri/maestria mexico/Descargas Profesores/2do semestre/econometria/Tarea 1/Metodos-Cuantitativos/test.csv")
 test
 
 x = test$Precio
 y= test$Superficie
 
 plot(x,y)
+
+y=mx+n
+
+
+#recta de regresion lineal 
+m=cov(y,x)/var(x)
+
+n=mean(y)-m*mean(x)
+
+m;n
+plot(x,y)
+abline(n,m,col="blue")
+
+resy=m*30+n
+resy
+
+reg1=lm(y~x)
+summary(reg1)
+
